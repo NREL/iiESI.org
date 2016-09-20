176df854511b99d987e3ca6fbe56cd487d5fe593
Amy Glickson <amy.glickson@nrel.gov>
2016-09-20 14:56:54 -0600

    new template SSI test

--- assets/js/jquery.custom.js
+++ assets/js/jquery.custom.js
@@ -495,8 +495,24 @@ $(document).ready(function() {
 
 });	// end pricing plans
 
+/* __________________ Add active nav class based on URL __________________*/
 
+$(function () {
+    setNavigation();
+});
 
+function setNavigation() {
+    var path = window.location.pathname;
+    path = path.replace(/\/$/, "");
+    path = decodeURIComponent(path);
+
+    $(".primary-nav a").each(function () {
+        var href = $(this).attr('href');
+        if (path.substring(0, href.length) === href) {
+            $(this).closest('li').addClass('active');
+        }
+    });
+}
 
 
 
